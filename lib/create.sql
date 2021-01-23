ee5045a554f7e07709751a2504f3bff3ba44dd27
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-23 17:52:58 -0600

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name Text,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+); 
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "F", "Brown", "Sanguine", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Black", "Melancholic ", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "White", "Phlegmatic", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "F", "Brown", "Choleric", 0 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "White", "Choleric", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "Brown", "Choleric", 0 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "White", "Phlegmatic", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, "M", "Black", "Melancholic", 0 );
+
